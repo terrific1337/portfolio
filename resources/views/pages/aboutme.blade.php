91b25bfdb56a8a07b3ffcaee2e4772094e4fa288
Anilcan Zorlu <anilcan@dispi.com>
2025-03-05 17:26:48 +0100

    5-3-2025 dag waar ik naar tandarts ging op stage commit

--- resources/views/pages/aboutme.blade.php
+++ resources/views/pages/aboutme.blade.php
@@ -1,5 +1,46 @@
 @extends('layouts.app')
 
 @section('content')
-about me
+<div class="aboutme-container">
+    <div class="aboutme-item">
+        <img src="" alt="selfie">
+        <p>
+            Hey, I'm Anilcan Zorlu, a passionate software developer with a focus on Laravel, PHP, and backend development. 
+            I love turning ideas into functional, scalable web applications. I'm always learning and improving, whether it's by optimizing database queries or exploring new tech stacks. 
+            Outside of coding, you might find me playing League of Legends or experimenting with new projects.
+        </p>
+    </div>
+
+    <div class="aboutme-item">
+        <img src="" alt="background">
+        <p>
+            My journey into programming started with my first year as a software developer student at the Veghel MBO School KW1C, 
+            where I was introduced to the basics of programming. During my studies, I primarily worked with HTML, CSS, PHP & MySQL. 
+            With my last internship, I picked up the Laravel Framework together with Vue. The portfolio you're viewing was created with Laravel & Vue as practice.
+        </p>
+    </div>
+
+    <div class="aboutme-item">
+        <img src="" alt="atm">
+        <p>
+            At the moment, I'm working on this portfolio to practice my skills with Laravel. 
+            I'm also doing an internship at Dispi / E-captain, where I am developing my portfolio and getting mentorship to improve my Laravel skills.
+        </p>
+    </div>
+
+    <div class="aboutme-item">
+        <img src="" alt="hobby">
+        <p>
+            In my free time, I enjoy playing League of Legends, which has been my competitive outlet for over 10 years. 
+            I consistently rank in the top 1%. I also love experimenting with new projects, like this portfolio, and always look for ways to learn and improve. 
+            Outside of gaming, I lift weights, watch anime, and read manga. Plus, I'm a proud owner of a Shiba Inu.
+        </p>
+    </div>
+
+    <div class="aboutme-item aboutme-links">
+        <p>📧 Email: <a href="mailto:AnilcZorlu@gmail.com">AnilcZorlu@gmail.com</a></p>
+        <p>🔗 <a href="https://www.linkedin.com/in/anilcanzorlu/" target="_blank">LinkedIn</a></p>
+        <p>💻 <a href="https://github.com/terrific1337?tab=repositories" target="_blank">GitHub</a></p>
+    </div>
+</div>
 @endsection

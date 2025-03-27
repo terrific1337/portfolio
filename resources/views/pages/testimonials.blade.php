4e5d8c73507917cf53039f9aece7bb81af281367
Anilcan Zorlu <anilcan@dispi.com>
2025-03-27 11:51:58 +0100

    testimonial update

--- resources/views/pages/testimonials.blade.php
+++ resources/views/pages/testimonials.blade.php
@@ -6,7 +6,6 @@
 <p class="section-subtitle">Don't just believe my word, here are others.</p>
 
 <div class="testimonial-container">
-    <h1 class="testimonial-title">{{ $pageTitle }}</h1>
 
     <div class="testimonial-grid">
         @foreach($testimonials as $testimonial)
